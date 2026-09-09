44ef57fc6237f07d90f970440b56559b170377ed
alessandrocinque <alessandro5cinque@gmail.com>
2026-09-09 16:11:30 +0100

    DFI-2519 Initial commit

---
+++ app/uk/gov/hmrc/disaaccountfrontend/viewmodels/checkAnswers/liaisonofficer/LiaisonOfficerPhoneSummary.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.disaaccountfrontend.viewmodels.checkAnswers.liaisonofficer
+
+import play.api.i18n.Messages
+import uk.gov.hmrc.disaaccountfrontend.controllers.liaisonofficers.routes.LiaisonOfficerPhoneNumberController
+import uk.gov.hmrc.disaaccountfrontend.models.CheckMode
+import uk.gov.hmrc.disaaccountfrontend.models.liaisonofficers.LiaisonOfficer
+import uk.gov.hmrc.govukfrontend.views.Aliases.{ActionItem, Actions, Key, SummaryListRow, Value}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
+
+object LiaisonOfficerPhoneSummary {
+  def row(officer: LiaisonOfficer)(implicit messages: Messages): Option[SummaryListRow] =
+    officer.phoneNumber.map { phone =>
+      SummaryListRow(
+        key = Key(Text(messages("liaisonOfficerPhoneNumber.checkYourAnswersLabel"))),
+        value = Value(Text(phone)),
+        actions = Some(
+          Actions(
+            items = Seq(
+              ActionItem(
+                href = LiaisonOfficerPhoneNumberController.onPageLoad(officer.id, CheckMode).url,
+                content = Text(messages("site.change")),
+                visuallyHiddenText = Some(messages("liaisonOfficerPhoneNumber.change.hidden"))
+              )
+            )
+          )
+        )
+      )
+    }
+
+}
