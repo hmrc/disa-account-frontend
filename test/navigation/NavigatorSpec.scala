37fdb04fa5064efb40f1e1e167d2b82764515f3c
alessandrocinque <alessandro5cinque@gmail.com>
2026-09-11 14:56:13 +0100

    DFI-2520 unit tests

--- test/navigation/NavigatorSpec.scala
+++ test/navigation/NavigatorSpec.scala
@@ -16,15 +16,16 @@
 
 package navigation
 
-import uk.gov.hmrc.disaaccountfrontend.controllers.liaisonofficers.routes.{LiaisonOfficerCommunicationController, LiaisonOfficerEmailController, LiaisonOfficerPhoneNumberController}
+import uk.gov.hmrc.disaaccountfrontend.controllers.liaisonofficers.routes.{AddedLiaisonOfficersController, LiaisonOfficerCommunicationController, LiaisonOfficerEmailController, LiaisonOfficerNameController, LiaisonOfficerPhoneNumberController}
 import uk.gov.hmrc.disaaccountfrontend.controllers.routes.{ChangeOfCircumstancesController, PeerToPeerPlatformController}
 import uk.gov.hmrc.disaaccountfrontend.controllers.orgdetails.routes.OrganisationTelephoneNumberController
 import uk.gov.hmrc.disaaccountfrontend.controllers.signatories.routes.{AddedSignatoryController, SignatoryCheckYourAnswersController, SignatoryJobTitleController, SignatoryNameController}
 import uk.gov.hmrc.disaaccountfrontend.models.YesNoAnswer.{No, Yes}
 import uk.gov.hmrc.disaaccountfrontend.models.{Answers, CheckMode, NormalMode, SessionUpdates}
 import uk.gov.hmrc.disaaccountfrontend.models.isaproducts.InnovativeFinancialProduct.{CrowdFundedDebentures, PeertopeerLoansUsingAPlatformWith36hPermissions}
+import uk.gov.hmrc.disaaccountfrontend.models.liaisonofficers.{LiaisonOfficer, LiaisonOfficers}
 import uk.gov.hmrc.disaaccountfrontend.models.pages.*
-import uk.gov.hmrc.disaaccountfrontend.models.pages.liaisonofficers.{LiaisonOfficerCommunicationPage, LiaisonOfficerEmailPage, LiaisonOfficerNamePage, LiaisonOfficerPhoneNumberPage}
+import uk.gov.hmrc.disaaccountfrontend.models.pages.liaisonofficers.{LiaisonOfficerCommunicationPage, LiaisonOfficerEmailPage, LiaisonOfficerNamePage, LiaisonOfficerPhoneNumberPage, RemoveLiaisonOfficerPage}
 import uk.gov.hmrc.disaaccountfrontend.models.pages.signatories.{RemoveSignatoryPage, SignatoryJobTitlePage, SignatoryNamePage}
 import uk.gov.hmrc.disaaccountfrontend.models.requests.DataRequest
 import uk.gov.hmrc.disaaccountfrontend.models.signatories.{Signatories, Signatory}
@@ -164,6 +165,31 @@ class NavigatorSpec extends BaseUnitSpec {
     "temporarily go from LiaisonOfficerCommunicationPage to change of circumstances in normal mode" in {
       navigator.nextPage(LiaisonOfficerCommunicationPage("liaison-officer-1"), mode = NormalMode) shouldBe
         ChangeOfCircumstancesController.onPageLoad()
+    }
+
+    "go from RemoveLiaisonOfficerPage to add a officers when none remain" in {
+      navigator.nextPage(
+        RemoveLiaisonOfficerPage(testLiaisonOfficerId),
+        Answers(liaisonOfficers = Some(LiaisonOfficers(Seq.empty)))
+      ) shouldBe
+        LiaisonOfficerNameController.onPageLoad(None, NormalMode)
+    }
+
+    "go from RemoveLiaisonOfficerPage to added liaison officers when a complete officer remains" in {
+      navigator.nextPage(
+        RemoveLiaisonOfficerPage(testLiaisonOfficerId),
+        Answers(liaisonOfficers = Some(testLiaisonOfficers))
+      ) shouldBe
+        AddedLiaisonOfficersController.onPageLoad()
+    }
+
+    "go from RemoveLiaisonOfficerPage to add a liaison officer when only incomplete records remain" in {
+      val incomplete = LiaisonOfficer("incomplete", fullName = Some("Incomplete"))
+
+      navigator.nextPage(
+        RemoveLiaisonOfficerPage(testLiaisonOfficerId),
+        Answers(liaisonOfficers = Some(LiaisonOfficers(Seq(incomplete))))
+      ) shouldBe LiaisonOfficerNameController.onPageLoad(None, NormalMode)
     }
 
     "temporarily go from LiaisonOfficerCommunicationPage to change of circumstances in check mode" in {
