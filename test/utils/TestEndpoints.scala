62bcff657e9f432edf8d4a90c4cb406f8492223d
Dan Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2026-09-16 14:07:17 +0100

    DFI-2533-DFI-2537: Added changes completed page for isa products and non-isa products (#28)

--- test/utils/TestEndpoints.scala
+++ test/utils/TestEndpoints.scala
@@ -56,6 +56,7 @@ trait TestEndpoints {
   val changeLiaisonOfficerCommunicationEndpoint: String =
     s"$accountFrontendRoutePrefix/change-liaison-officer-communication"
   val changeOfCircumstancesEndpoint: String             = s"$accountFrontendRoutePrefix/change-of-circumstances"
+  val changesCompletedEndpoint: String                  = s"$accountFrontendRoutePrefix/changes-completed"
   val fcaArticlesEndpoint: String                       = s"$accountFrontendRoutePrefix/fca-articles"
   val keepAliveEndpoint: String                         = s"$accountFrontendRoutePrefix/refresh-session"
   val signOutEndpoint: String                           = s"$accountFrontendRoutePrefix/sign-out"
